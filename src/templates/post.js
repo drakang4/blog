29fcc726b2a02d989984b7bc753245c2a40c119e
Heeryong Kang <drakang4@users.noreply.github.com>
2018-03-29 23:26:17 +0900

    Updated siteURL as config

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,24 +3,27 @@ import Helmet from 'react-helmet';
 import { Container, Row, Col } from 'reactstrap';
 
 const PostTemplate = ({ data }) => {
-  const { markdownRemark } = data;
+  const { markdownRemark, site } = data;
+  
   const { frontmatter, fields, html, timeToRead } = markdownRemark;
-
   const { title, date, description, thumbnail, tags } = frontmatter;
+  
+  const { siteMetadata } = site;
+  const { siteURL } = siteMetadata;
 
   return (
     <article className="my-4">
       <Helmet>
         <title>{title}</title>
         <meta name="description" content={description} />
-        <link rel="canonical" href={`https://www.heeryongkang.me${fields.slug}`} />
+        <link rel="canonical" href={`${siteURL}${fields.slug}`} />
         {/* Facebook Open Graph */}
         <meta property="fb:app_id" content="191342008318335" />
         <meta property="og:type" content="article" />
-        <meta property="og:url" content={`https://www.heeryongkang.me${fields.slug}`} />
+        <meta property="og:url" content={`${siteURL}${fields.slug}`} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content={`https://www.heeryongkang.me${thumbnail}`} />
+        <meta property="og:image" content={`${siteURL}${thumbnail}`} />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="article:published_time" content={date} />
@@ -28,10 +31,10 @@ const PostTemplate = ({ data }) => {
         <meta property="article:author" content="Heeryong Kang" />
         {/* Twitter Card */}
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content={`https://www.heeryongkang.me${fields.slug}`} />
+        <meta name="twitter:url" content={`${siteURL}${fields.slug}`} />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={`https://www.heeryongkang.me${thumbnail}`} />
+        <meta name="twitter:image" content={`${siteURL}${thumbnail}`} />
       </Helmet>
       <Container>
         <Row>
@@ -57,6 +60,11 @@ export default PostTemplate;
 
 export const pageQuery = graphql`
   query BlogPostByID($id: String!) {
+    site {
+      siteMetadata {
+        siteURL
+      }
+    }
     markdownRemark(id: { eq: $id }) {
       id
       html

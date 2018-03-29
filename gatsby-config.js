864b94ec05f8c215930b22ad8d019d263f18dd3a
Heeryong Kang <drakang4@users.noreply.github.com>
2018-03-29 20:11:39 +0900

    Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,5 +27,13 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-116620737-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+      },
+    },
   ],
 };

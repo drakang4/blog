c6caa34690472036046e28ffd3e2079905c8fe87
Heeryong Kang <drakang4@users.noreply.github.com>
2018-10-29 19:00:16 +0900

    Use react i18next provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,14 @@
  */
 
 // You can delete this file if you're not using it
+import React from 'react';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './src/i18n/i18n';
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <I18nextProvider i18n={i18n} initialLanguage="ko">
+      {element}
+    </I18nextProvider>
+  );
+};

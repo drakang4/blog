e918c8f4cd3152e3070fed439f752df852021ac4
Heeryong Kang <drakang4@users.noreply.github.com>
2018-10-29 22:27:32 +0900

    Keep locale on change

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,9 +10,5 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from './src/i18n/i18n';
 
 export const wrapRootElement = ({ element }) => {
-  return (
-    <I18nextProvider i18n={i18n} initialLanguage="ko">
-      {element}
-    </I18nextProvider>
-  );
+  return <I18nextProvider i18n={i18n}>{element}</I18nextProvider>;
 };

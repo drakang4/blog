f21c401dd4d8afbf763496aebabd007f588caf84
Heeryong Kang <drakang4@users.noreply.github.com>
2018-10-29 11:37:10 +0900

    Wait i18n render

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -30,7 +30,7 @@ i18n
 
     // react i18next special options (optional)
     react: {
-      wait: false,
+      wait: true,
       bindI18n: 'languageChanged loaded',
       bindStore: 'added removed',
       nsMode: 'default',

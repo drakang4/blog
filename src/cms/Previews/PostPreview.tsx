bf6f2f9fa7a85133189f9454e7c02f4b9dc7f9d9
Heeryong <drakang4@gmail.com>
2019-02-05 12:18:07 +0900

    Update Neltify CMS

---
+++ src/cms/Previews/PostPreview.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PostPreview: React.FC = () => {
+  return <div />;
+};
+
+export default PostPreview;
--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,3 +1,1 @@
-import react from 'react';
 import CMS from 'netlify-cms';
-import 'netlify-cms/dist/cms.css';

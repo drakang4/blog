b8e3f3a00912730f90f1a18c6e436dd1a93ca271
Heeryong <drakang4@gmail.com>
2021-02-15 01:54:31 +0900

    change utterances theme on context change

--- src/components/Utterances.tsx
+++ src/components/Utterances.tsx
@@ -1,15 +1,37 @@
 import React, { useEffect, useRef } from 'react';
+import useTheme from '../hooks/useTheme';
 
 const Utterances = () => {
+  const initialzied = useRef(false);
   const wrapper = useRef<HTMLDivElement>(null);
+  const { theme } = useTheme();
 
   useEffect(() => {
+    if (!theme) {
+      return;
+    }
+
+    const utterancesTheme = theme === 'dark' ? 'github-dark' : 'github-light';
+
+    if (initialzied.current) {
+      const message = {
+        type: 'set-theme',
+        theme: utterancesTheme,
+      };
+      const frame = document.querySelector<HTMLIFrameElement>(
+        '.utterances-frame',
+      );
+      frame?.contentWindow?.postMessage(message, 'https://utteranc.es');
+
+      return;
+    }
+
     const utterances = document.createElement('script');
     const utterancesConfig: { [key: string]: any } = {
       src: 'https://utteranc.es/client.js',
       repo: 'drakang4/blog',
       'issue-term': 'pathname',
-      theme: 'preferred-color-scheme',
+      theme: utterancesTheme,
       crossorigin: 'anonymous',
       async: true,
     };
@@ -19,9 +41,10 @@ const Utterances = () => {
     });
 
     wrapper.current?.appendChild(utterances);
-  }, []);
+    initialzied.current = true;
+  }, [theme]);
 
-  return <div ref={wrapper} className="mx-auto max-w-3xl py-4 lg:py-0 " />;
+  return <div ref={wrapper} className="mx-auto max-w-3xl py-4 lg:py-0" />;
 };
 
 export default Utterances;
